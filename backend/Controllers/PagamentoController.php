78054f033073345e8164ec578d2aa90c8535ab52
victorGAPsilva <victor.gapsilva1@senac.edu.br>
2025-10-03 11:31:54 -0300

    commit antes do merge

---
+++ backend/Controllers/PagamentoController.php
@@ -0,0 +1,26 @@
+<?php
+namespace Controllers;
+use Models\Pagamento;
+class PagamentoController{
+    private $pagamento;
+    public function __construct($db){
+        $this->pagamento = new Pagamento($db);
+    }
+    public function viewListarTodosPagamentos(){
+        $resultado = $this->pagamento->buscarTodos();
+        var_dump($resultado);
+    }
+    public function viewListarPagamentoPorId($id){
+        $resultado = $this->pagamento->buscarPorId($id);
+        var_dump($resultado);
+    }
+    public function viewInserirPagamento($pedido_id, $metodo, $status_pagamento_id, $valor_total){
+        $resultado = $this->pagamento->inserir($pedido_id, $metodo, $status_pagamento_id, $valor_total);
+        var_dump($resultado);
+        
+    } 
+    public function viewAtualizarPagamento($id, $metodo, $status_pagamento_id, $valor_total){
+        $resultado = $this->pagamento->atualizar($id, $metodo, $status_pagamento_id, $valor_total);
+        var_dump($resultado);
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'app/tadala',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '920f515df52ecf133ce53a86a13c3f848f1fdb3f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'app/tadala' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '920f515df52ecf133ce53a86a13c3f848f1fdb3f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
