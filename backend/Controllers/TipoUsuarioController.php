eb6774f6bb605ea69f761326f742e8bbe873c437
victorGAPsilva <victor.gapsilva1@senac.edu.br>
2025-10-17 10:29:15 -0300

    NOVA BRANCH

---
+++ backend/Controllers/TipoUsuarioController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Tadala\Controllers;
+
+use App\Tadala\Models\TipoUsuario;
+use App\Tadala\Database\Database;
+use App\Tadala\Core\View;
+
+
+class TipoUsuarioControler {
+    public $tipoUsuario;
+    public $db;
+
+    public function __construct($db){
+        $this->db = Database::getInstance();
+        $this->tipoUsuario = new TipoUsuario($db);
+    }
+
+    public function index(){
+        $resultado = $this->tipoUsuario->buscarTodosTipoUsuario();
+        View::render("tipoUsuario/index", ["tipoUsuario" => $resultado]);
+
+    }
+    public function viewListarTipoUsuario($pagina = 1)
+{
+    $pagina = isset($pagina) ? $pagina : 1;
+    $dados = $this->tipoUsuario->paginacaoTipoUsuario($pagina);
+    $total = $this->tipoUsuario->totalTipoUsuario();
+    $total_inativos = $this->tipoUsuario->totalTipoUsuarioInativos();
+    $total_ativos = $this->tipoUsuario->totalTipoUsuarioAtivos();
+
+    View::render("usuario/index", 
+    [
+    "usuarios"=> $dados['data'],
+     "total_usuarios"=> $total['total'],
+     "total_inativos" => $total_inativos['total'],
+     "total_ativos" => $total_ativos['total'],
+     'paginacao' => $dados
+    ] 
+    );
+
+
+}
+
+
+    public function mostrar($id){
+        $this->tipoUsuario->buscarPorIdTipoUsuario($id);
+    }
+
+    public function criar($descricao){
+        $resultado = $this->tipoUsuario->inserirTipoUsuario($descricao);
+        
+    }
+
+    public function atualizar($id, $descricao){
+        $resultado = $this->tipoUsuario->atualizarTipoUsuario($id, $descricao);
+       
+    }
+}
+?>
