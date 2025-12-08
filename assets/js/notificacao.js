ae9d458e1952f2de8ab938b96320bca86783610b
Guest4hu <gustavoqwe25@gmail.com>
2025-12-08 11:57:46 -0300

    Começando a reestrutura o codigo e organizar as pastas

--- assets/js/notificacao.js
+++ assets/js/notificacao.js
@@ -1,3 +1,20 @@
 const notificacao = new Audio('/assets/audio/notificacao.mp3');
 const negar = new Audio('/assets/audio/negacacao.mp3');
 const confirmar = new Audio('/assets/audio/confirm.mp3');
+
+
+
+function notificar(icon,titulo,){
+    Swal.fire({
+         position: "top-end",
+         icon: icon,
+         title: titulo,
+         showConfirmButton: false,
+         timer: 3000,
+         timerProgressBar: true,
+      });
+}
+
+
+
+
