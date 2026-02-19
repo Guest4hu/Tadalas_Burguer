32f34e0b8c636d7378ab07e3e7b9a0d38aa1c126
ThinkPadE480 <matheusgois.pessoal@gmail.com>
2026-02-19 03:44:20 -0300

    Classe separada pra tratar a renderização da tela de carregamento

---
+++ assets/js/loading.js
@@ -0,0 +1,29 @@
+function telaDeCarregamento() {
+
+    let limit = document.body.childNodes.length
+    let node
+
+    for (let i = 0; i < limit; i++) {
+        if (document.body.childNodes[i].nodeName !== '#text') {
+            node = document.body.childNodes[i]
+            break
+        }
+    }
+
+    node.classList.add('blur')
+
+    const burguer = document.createElement('div')
+    burguer.className = 'ham'
+    burguer.innerHTML =
+        `
+        <img src="/assets/img/loading.png" id="burguer">
+    `
+
+    document.body.appendChild(burguer)
+}
+
+telaDeCarregamento()
+
+// setTimeout(() => {
+//     history.go(-2)
+// }, 2000)
--- backend/Core/Loading.php
+++ backend/Core/Loading.php
@@ -5,29 +5,51 @@ namespace App\Tadala\Core;
 
 class Loading
 {
+    private static $temporaryPath = __DIR__ . "/../Views/templates/temporary/load.html";
+    private static $cssPath = __DIR__ . '/../../assets/css/loading.css';
+    private static $jsPath = __DIR__ . '/../../assets/js/loading.js';
+
     public static function add($file)
     {
         $html = file_get_contents(__DIR__ . "/../Views/templates/{$file}.php");
-        // print_r(htmlspecialchars($html));
 
-        $dom = new \DOMDocument();
-
-        @$dom->loadHTML($html);
-
-        $title = $dom->getElementById('titulo');
-
-        echo $title->textContent;
-        exit;
+        self::render(
+                        self::addLoadingAssets($html)
+                    );
     }
 
-    private static function getUrl()
+    private static function parseFiles()
     {
-        $is_https = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on');
+        $js = '<script>' . file_get_contents(self::$jsPath) . '</script>';
 
-        $scheme = $is_https ? 'https' : 'http';
-        $host = $_SERVER['HTTP_HOST'];
-        $uri = $_SERVER['REQUEST_URI'] ?? '';
+        $css = file_get_contents(self::$cssPath);
 
-        return $scheme . '://' . $host . $uri;
+        return [$js, $css];
+    }
+
+    private static function addLoadingAssets($html)
+    {
+        [$js, $css] = self::parseFiles();
+
+        $before = ['</head>', '</body>'];
+
+        $cssPosition = strpos($html, $before[0]) - strlen(' </style> ');
+
+        $withCss = substr_replace($html, $css, $cssPosition, 0);
+
+        $jsPosition = strpos($withCss, $before[1]);
+
+        return substr_replace($withCss, $js, $jsPosition, 0);
+    }
+    
+    private static function render($page_content) {
+
+        $handle = fopen(self::$temporaryPath, 'w');
+
+        fwrite($handle, $page_content);
+
+        fclose($handle);
+
+        require_once self::$temporaryPath;
     }
 }
